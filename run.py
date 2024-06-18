027ae97e4e447bb70bb206e9ed8939356945fe3b
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-06-18 14:54:39 +0000

    Add function to generate game grid.

--- run.py
+++ run.py
@@ -5,10 +5,25 @@ Global variables
 """
 
 X_AXIS = 10	
-Y_AXIS = 10
+Y_AXIS = 5
 
 def generate_grid(x, y):
-    print(x, y)
-    
+    """
+    generates a list of lists based on x and y inputs.
+    """
+    y_axis = []
+    i = 0
+    while y > i:
+        i +=1
+        x_axis = []
+        j = 0
+        while x > j:
+            x_axis.append('^')
+            j +=1
+        y_axis.append(x_axis)
 
-generate_grid(X_AXIS, Y_AXIS)
+    return y_axis
+   
+
+ocean = generate_grid(X_AXIS, Y_AXIS)
+print(ocean)
