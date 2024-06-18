5c175d32d111840968068f2f87a4f4014a77c7ab
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-06-18 14:44:28 +0000

    Initial commit.

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+"""
+Global variables
+"""
+
+X_AXIS = 10	
+Y_AXIS = 10
+
+def generate_grid(x, y):
+    print(x, y)
+    
+
+generate_grid(X_AXIS, Y_AXIS)
