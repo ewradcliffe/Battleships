3692615bdda2ab98dab9e087f48beb27838237c0
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-06-19 12:05:54 +0000

    Add function to add ships at random to grid.

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ Global variables
 
 X_AXIS = 10	
 Y_AXIS = 5
+fleet_size = 3
 
 def generate_grid(x, y):
     """
@@ -25,6 +26,21 @@ def generate_grid(x, y):
     return y_axis
    
 
+def add_ships(game_grid, ships):
+    """
+    Add ships to the grid. Randomly selects an index for the y axis and x axis and inserts an '0'
+    """
+    while ships > 0:
+            random_x_axis = random.randint(0, X_AXIS-1)
+            random_y_axis = random.randint(0, Y_AXIS-1)
+            if game_grid[random_y_axis][random_x_axis] == '0':
+                continue
+            else:
+                game_grid[random_y_axis][random_x_axis] = '0'
+                ships -= 1
+
+    return game_grid
+
 def display_battlespace(grid):
     """
     Displays the game area in a viewer freindly format.
@@ -43,5 +59,8 @@ def display_battlespace(grid):
 
 
 game_grid = generate_grid(X_AXIS, Y_AXIS)
-display = display_battlespace(game_grid)
+
+ships = add_ships(game_grid, fleet_size)
+
+display = display_battlespace(ships)
 print(display)
